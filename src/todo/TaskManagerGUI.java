c3c25f6516dca1d804d92c48a61db68bc73c16f6
Sidra-Hayat <sp24-bcs-093@students.cuisahiwal.edu.pk>
2025-12-06 13:01:48 +0500

    “Added Task and TaskLinkedList classes”

--- src/todo/TaskManagerGUI.java
+++ src/todo/TaskManagerGUI.java
@@ -1,4 +1,24 @@
 package todo;
 
-public class TaskManagerGUI {
+import javax.swing.*;
+
+public class TaskManagerGUI extends JFrame {
+    private TaskLinkedList taskList = new TaskLinkedList();
+    private DefaultListModel<Task> listModel = new DefaultListModel<>();
+    private JList<Task> taskJList = new JList<>(listModel);
+
+    public TaskManagerGUI() {
+        setTitle("ToDo List Manager");
+        setSize(500, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JScrollPane scrollPane = new JScrollPane(taskJList);
+        add(scrollPane);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new TaskManagerGUI();
+    }
 }
